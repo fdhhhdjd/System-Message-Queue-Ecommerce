63e0a74fc4003c81231c479566180a87cb175b49
fdhhhdjd <nguyentientai10@gmail.com>
2023-08-15 16:23:01 +0700

    #3 Setup Format Code

--- src/dbs/init.rabbit.js
+++ src/dbs/init.rabbit.js
@@ -1,56 +1,55 @@
-"use strict";
+'use strict';
 
 //* LIBRARY
-const amqp = require("amqplib");
-const path = require("path");
+const amqp = require('amqplib');
+const path = require('path');
 
 //* USED LIBRARY
-require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 const connectToRabbitMQ = async () => {
-  try {
-    const connection = await amqp.connect(process.env.HOST_RABBIT);
+    try {
+        const connection = await amqp.connect(process.env.HOST_RABBIT);
 
-    if (!connection) throw new Error("Couldn't connect to RabbitMQ");
+        if (!connection) throw new Error("Couldn't connect to RabbitMQ");
 
-    const channel = await connection.createChannel();
+        const channel = await connection.createChannel();
 
-    return {
-      channel,
-      connection,
-    };
-  } catch (error) {
-    console.error("Get started", error);
-    throw error;
-  }
+        return {
+            channel,
+            connection,
+        };
+    } catch (error) {
+        console.error('Get started', error);
+        throw error;
+    }
 };
 
 const connectToRabbitMQForTest = async () => {
-  try {
-    const { channel, connection } = await connectToRabbitMQ();
+    try {
+        const { channel, connection } = await connectToRabbitMQ();
 
-    console.log(channel);
-    // Publish message to queue
-    const queue = "test-queue";
-    const message = "Hello Tai Dev test RabbitMQ";
+        // Publish message to queue
+        const queue = 'test-queue';
+        const message = 'Hello Tai Dev test RabbitMQ';
 
-    // Create queue
-    await channel.assertQueue(queue);
+        // Create queue
+        await channel.assertQueue(queue);
 
-    // Send message to queue
-    await channel.sendToQueue(queue, Buffer.from(message));
+        // Send message to queue
+        await channel.sendToQueue(queue, Buffer.from(message));
 
-    // Close connection to queue
-    await connection.close();
+        // Close connection to queue
+        await connection.close();
 
-    console.log("Send RabbitMQ message success");
-  } catch (error) {
-    console.error("Connect Fail", error);
-    throw error;
-  }
+        console.error('Send RabbitMQ message success');
+    } catch (error) {
+        console.error('Connect Fail', error);
+        throw error;
+    }
 };
 
 module.exports = {
-  connectToRabbitMQ,
-  connectToRabbitMQForTest,
+    connectToRabbitMQ,
+    connectToRabbitMQForTest,
 };
--- src/tests/rabbitmq.test.js
+++ src/tests/rabbitmq.test.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 //* DB
-const { connectToRabbitMQForTest } = require("../dbs/init.rabbit");
+const { connectToRabbitMQForTest } = require('../dbs/init.rabbit');
 
-describe("RabbitMQ Connection", () => {
-  it("should connect to successful rabbitMQ", async () => {
-    const result = await connectToRabbitMQForTest();
-    expect(result).toBeUndefined();
-  });
+describe('RabbitMQ Connection', () => {
+    it('should connect to successful rabbitMQ', async () => {
+        const result = await connectToRabbitMQForTest();
+        expect(result).toBeUndefined();
+    });
 });
